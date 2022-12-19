46387a6a6dccea90134e3ec2c9d8f9027312887f
Keysd <keysd290@gmail.com>
2022-12-20 02:25:05 +0700

    new code

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const route = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use('/static', express.static(path.join(__dirname, '/static')));
 console.log(__dirname);
